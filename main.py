6f7389e0fbbf798397dbb5bb5023e0c786e6bf8d
ubuntu <foenix208@gmail.com>
2025-02-14 21:31:13 +0100

    start main

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("hello world")
+import requests
+import sys
+
+def get_html_requests(url):
+    r = requests.get(url)
+    if r.status_code == 200:
+        return r.text
+    else:
+        print(f"[  {r.status_code}  ] Erreur requets a {url}")
+        return r.status_code
+    
+def main(argc,argv):
+    return 0;
+
+if __name__ == '__main__':
+    print('''      
+ ██████╗██╗   ██╗██████╗ ███████╗██████╗ ██████╗  █████╗ ██╗████████╗
+██╔════╝╚██╗ ██╔╝██╔══██╗██╔════╝██╔══██╗██╔══██╗██╔══██╗██║╚══██╔══╝
+██║      ╚████╔╝ ██████╔╝█████╗  ██████╔╝██████╔╝███████║██║   ██║   
+██║       ╚██╔╝  ██╔══██╗██╔══╝  ██╔══██╗██╔══██╗██╔══██║██║   ██║   
+╚██████╗   ██║   ██████╔╝███████╗██║  ██║██████╔╝██║  ██║██║   ██║   
+ ╚═════╝   ╚═╝   ╚═════╝ ╚══════╝╚═╝  ╚═╝╚═════╝ ╚═╝  ╚═╝╚═╝   ╚═╝                                                           
+    ''')
+    main(len(sys.argv),sys.argv) 
